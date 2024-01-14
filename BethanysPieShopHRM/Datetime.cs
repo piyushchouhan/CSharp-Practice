8dcaad3d94cd11c488f788ae408c3cb5de79944c
Piyush Chauhan <piyush9695@gmail.com>
2024-01-14 15:29:57 +0530

    code added

---
+++ BethanysPieShopHRM/Datetime.cs
@@ -0,0 +1,24 @@
+﻿DateTime hireDate = new DateTime(2024, 1,1 , 12, 00, 0);
+Console.WriteLine(hireDate);
+
+DateTime exitDate = new DateTime(2024, 07, 07);
+Console.WriteLine(exitDate);
+
+DateTime startDate = hireDate.AddDays(8);
+Console.WriteLine(startDate);
+
+DateTime startHour = DateTime.Now;
+TimeSpan timeSpent = new TimeSpan(8, 35, 8);
+
+DateTime endHour = startHour.Add(timeSpent);
+
+Console.WriteLine(startHour); 
+Console.WriteLine(endHour);
+
+Console.WriteLine(startHour.ToLongDateString());
+Console.WriteLine(endHour.ToShortDateString());
+
+DateTime endDate = DateTime.Today;
+Console.WriteLine(endDate);
+
+Console.ReadLine();
---
+++ BethanysPieShopHRM/TypeConversion.cs
@@ -0,0 +1,16 @@
+﻿int numberOfHourWorked = 15;
+Console.WriteLine(numberOfHourWorked);
+
+long veryLongMonth = numberOfHourWorked;
+Console.WriteLine(veryLongMonth);
+
+double d = 1245854.5;
+Console.WriteLine(d);
+
+int x = (int)d;
+Console.WriteLine(x);
+
+int intVeryLongMonth = (int)veryLongMonth;
+Console.WriteLine(intVeryLongMonth);
+
+Console.WriteLine();
